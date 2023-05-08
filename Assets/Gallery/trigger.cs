079a5ab984f6444736e0aef6bdeaac4088dd48b4
CaicaicaiLG <1829651507@qq.com>
2023-05-08 10:30:25 +0800

    debug
    the position of timeline animation,
    the appearance of text, add the button to start interface, etc.

--- Assets/Gallery/trigger.cs
+++ Assets/Gallery/trigger.cs
@@ -11,6 +11,8 @@ public class trigger : MonoBehaviour
     //GameObject button_man;
     public Button man;
     public Button enter;
+    public GameObject txt;
+    public GameObject light;
 
     public GameObject girl;
     private PlayableDirector director=null;
@@ -22,30 +24,13 @@ public class trigger : MonoBehaviour
         //按钮消失
         man.transform.gameObject.SetActive(false);
         enter.transform.gameObject.SetActive(false);
+        //文本消失
+		txt.transform.gameObject.SetActive(false);
         //数字人消失
-        /*
         //girl.transform.GetChild(0);
-        Material mm1 = girl.transform.GetChild(1).GetComponent<Renderer>().material;
-        mm1.SetFloat("_Dissolve",0);
-        Material mm2 = girl.transform.GetChild(2).GetComponent<Renderer>().material;
-        mm2.SetFloat("_Dissolve",0);
-        Material mm3 = girl.transform.GetChild(3).GetComponent<Renderer>().material;
-        mm3.SetFloat("_Dissolve",0);
-        Material mm4 = girl.transform.GetChild(4).GetComponent<Renderer>().material;
-        mm4.SetFloat("_Dissolve",0);
-        Material mm5 = girl.transform.GetChild(5).GetComponent<Renderer>().material;
-        mm5.SetFloat("_Dissolve",0);
-        Material mm6 = girl.transform.GetChild(6).GetComponent<Renderer>().material;
-        mm6.SetFloat("_Dissolve",0);
-        Material mm7 = girl.transform.GetChild(7).GetComponent<Renderer>().material;
-        mm7.SetFloat("_Dissolve",0);
-        Material mm8 = girl.transform.GetChild(8).GetComponent<Renderer>().material;
-        mm8.SetFloat("_Dissolve",0);
-        Material mm9 = girl.transform.GetChild(9).GetComponent<Renderer>().material;
-        mm9.SetFloat("_Dissolve",0);
-        */
+        girl.transform.position += Vector3.forward * 2;
         //灯光消失
-        //girl.transform.GetChild(10)
+        light.transform.gameObject.SetActive(false);
         //获取组件
         director=girl.transform.GetComponent<PlayableDirector>();
         
@@ -81,14 +66,18 @@ public class trigger : MonoBehaviour
         mm8.SetFloat("_Dissolve",4);
         mm9.SetFloat("_Dissolve",4);
         */
-        //开启动画
-        //girl.GetComponent<PlayableDirector>().Play();
+        girl.transform.position -= Vector3.forward * 2;
+        light.transform.gameObject.SetActive(true);
         director.Play();
-        
+        man.transform.gameObject.SetActive(false);
+        enter.transform.gameObject.SetActive(false);
+        txt.transform.gameObject.SetActive(true);
     }
     public void OnStartButtonClick_enter()
     {
         Debug.Log("点击enter按钮");//只能运行时看到输出，可以换其他点击事件。
+        man.transform.gameObject.SetActive(false);
+        enter.transform.gameObject.SetActive(false);
         SceneManager.LoadScene("Water/scene");
     }
 
@@ -111,20 +100,21 @@ public class trigger : MonoBehaviour
         Debug.Log(girl.transform.GetChild(1));
         
         //按钮出现
-
+        man.transform.gameObject.SetActive(true);
+        enter.transform.gameObject.SetActive(true);
         
     }
     void OnTriggerStay(Collider other)
     {
         Debug.Log("触发器检测中:" + other.gameObject.name);
-        man.transform.gameObject.SetActive(true);
-        enter.transform.gameObject.SetActive(true);
+        
     }
     void OnTriggerExit(Collider other)
     {
         Debug.Log("触发器结束:" + other.gameObject.name);
         man.transform.gameObject.SetActive(false);
         enter.transform.gameObject.SetActive(false);
+        txt.transform.gameObject.SetActive(false);
     }
 
 }
--- Assets/SimpleCameraController.cs
+++ Assets/SimpleCameraController.cs
@@ -10,6 +10,7 @@
      public float rotation_V_speed =1 ;
      public float max_up_angle =1 ;              //越大，头抬得越高
      public float max_down_angle =1 ;           //越小，头抬得越低
+     public float rotationSpeed = 50.0f;
  
      private float current_rotation_H;  //水平旋转结果
      private float current_rotation_V;  //垂直旋转结果
@@ -21,38 +22,37 @@
      }
  
      // Update is called once per frame
-     void Update()
-     {
+    void Update()
+    {
          // 移动
-         if (Input.GetKey(KeyCode.A)) //左移
-         {
-             transform.Translate(Vector3.left * speed * Time.deltaTime);
-         }
-         if (Input.GetKey(KeyCode.D)) //右移
-         {
-             transform.Translate(Vector3.right * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A)) //左移
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D)) //右移
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
  
-         }
-         if (Input.GetKey(KeyCode.W)) //前移
-         {
-             transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W)) //前移
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
  
-         }
-         if (Input.GetKey(KeyCode.S)) //后移
-         {
-             transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S)) //后移
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
  
-         }
- 
-         // 缩放
-         if (Input.GetAxis("Mouse ScrollWheel") != 0)
-         {
-             //获取鼠标滚轮的滑动量
-             float wheel = Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime ;
- 
-             //改变相机的位置
-             this.transform.Translate(Vector3.forward * wheel);
-         }
-     }
+        }
+         // 旋转
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        }
+    }
  }
 
